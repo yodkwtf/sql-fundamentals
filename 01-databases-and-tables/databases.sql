ec17aad49e31bc7bf60216deed80aa47ac2c596d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-01-06 15:24:43 +0530

    working with databases

---
+++ 01-databases-and-tables/databases.sql
@@ -0,0 +1,17 @@
+-- show list of available databases
+SHOW DATABASES;
+
+-- create a database
+CREATE DATABASE learnSQL;
+
+CREATE DATABASE petStore;
+
+-- drop database
+DROP DATABASE learnSQL;
+
+-- select a database to work with
+USE petStore;
+
+-- show the current selected database
+SELECT
+  DATABASE ();
---
+++ test.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE cats;
+
+
+DROP DATABASE cats;
