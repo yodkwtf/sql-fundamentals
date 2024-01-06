81897f8d13b40ae10118d63b49e8ecdbfd79273c
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-01-07 02:31:59 +0530

    repo setup

--- test.sql
+++ test.sql
@@ -1,4 +1,18 @@
 CREATE DATABASE cats;
 
 
+USE cats;
+
+
+CREATE TABLE table_name(name VARCHAR(50), amount INT);
+
+
+INSERT INTO
+  table_name(name, amount)
+VALUES
+  ('Bob', 200),
+  ('Bob', 200),
+  ('Bob', 200);
+
+
 DROP DATABASE cats;
