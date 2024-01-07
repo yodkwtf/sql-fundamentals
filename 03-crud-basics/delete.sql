0e87893d8c4ac1c4f46541b5455c24cde0c581c4
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-01-08 00:57:42 +0530

    delete statements

---
+++ 03-crud-basics/delete.sql
@@ -0,0 +1,28 @@
+SELECT
+  *
+FROM
+  cakes;
+
+
+--- delete
+DELETE FROM
+  cakes
+WHERE
+  price > 450;
+
+
+SELECT
+  *
+FROM
+  cakes;
+
+
+-- delete all data
+DELETE FROM
+  cakes;
+
+
+SELECT
+  *
+FROM
+  cakes;
