a65981007df542317b8f1bbd84c0e7b6982ec898
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-01-14 21:57:05 +0530

    group by

---
+++ 06-aggregate-functions/group-by.sql
@@ -0,0 +1,41 @@
+-- group data by author's last name
+SELECT
+  author_lname
+FROM
+  books
+GROUP BY
+  author_lname;
+
+
+-- get the count of books after grouping them based on author's last name
+SELECT
+  author_lname,
+  COUNT(*)
+FROM
+  books
+GROUP BY
+  author_lname;
+
+
+-- group by unique authors and make the output a bit cleaner
+SELECT
+  CONCAT(author_fname, ' ', author_lname) AS author,
+  COUNT(*) AS books_written
+FROM
+  books
+GROUP BY
+  author
+ORDER BY
+  books_written DESC;
+
+
+-- group by released year
+SELECT
+  released_year,
+  COUNT(*)
+FROM
+  books
+GROUP BY
+  released_year
+ORDER BY
+  released_year;
