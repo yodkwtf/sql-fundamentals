f742e227258eddc5a6d8362cbe9e440153bea739
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-02-01 22:53:12 +0530

    most complex challenge was ez

--- 10-relationships-and-joins/many-to-many/challenge-06.sql
+++ 10-relationships-and-joins/many-to-many/challenge-06.sql
@@ -17,3 +17,20 @@ FROM
 
 
 -- # SOLUTION
+SELECT
+  first_name,
+  last_name,
+  COUNT(rating) AS COUNT,
+  IFNULL(MIN(rating), 0) AS MIN,
+  IFNULL(MAX(rating), 0) AS MAX,
+  IFNULL(AVG(rating), 0) AS AVG,
+  CASE
+    WHEN COUNT(rating) > 0 THEN "ACTIVE"
+    ELSE "INACTIVE"
+  END AS STATUS
+FROM
+  reviewers
+  LEFT JOIN reviews ON reviews.reviewer_id = reviewers.id
+GROUP BY
+  first_name,
+  last_name;
