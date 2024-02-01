9b1c5cce312e8fd3c03b2dcd39fbc8035874a80d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-02-01 23:05:14 +0530

    final challenge done

--- 10-relationships-and-joins/many-to-many/challenge-07.sql
+++ 10-relationships-and-joins/many-to-many/challenge-07.sql
@@ -16,22 +16,26 @@ FROM
   reviews;
 
 
--- # SOLUTIONSELECT
-*
-FROM
-  series;
-
-
-SELECT
-  *
-FROM
-  reviewers;
-
-
-SELECT
-  *
-FROM
-  reviews;
-
-
 -- # SOLUTION
+SELECT
+  title,
+  rating,
+  CONCAT(first_name, " ", last_name) AS reviewer
+FROM
+  reviews
+  JOIN series ON reviews.series_id = series.id
+  JOIN reviewers ON reviewers.id = reviews.reviewer_id
+ORDER BY
+  title;
+
+
+SELECT
+  CONCAT(first_name, " ", last_name) AS reviewer,
+  title,
+  rating
+FROM
+  reviews
+  JOIN series ON reviews.series_id = series.id
+  JOIN reviewers ON reviewers.id = reviews.reviewer_id
+ORDER BY
+  title;
